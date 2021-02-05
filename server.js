e1a94c553260949d9a6864c0634ac63ca7192c55
Paty Alves <patriciaalves10@gmail.com>
2021-02-05 09:55:30 -0300

    node

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
-var static = require( 'node-static' );
-var file = new static.Server();
-require( 'http' ).createServer( function ( request, response ) {
-    request.addListener( 'end', function () {
-        file.serve( request, response );
-    } ).resume();
-} ).listen( process.env.PORT || 3000 );
+// create an express app
+const express = require( "express" )
+const app = express()
+
+// use the express-static middleware
+app.use( express.static( "public" ) )
+
+// define the first route
+app.get( "/", function ( req, res ) {
+    res.send( "<h1>Hello World!</h1>" )
+} )
+
+// start the server listening for requests
+app.listen( process.env.PORT || 3000,
+    () => console.log( "Server is running..." ) );
