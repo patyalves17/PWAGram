b49b48e124bb8afe45f5c76be807fe0a6cd9683e
Paty Alves <patriciaalves10@gmail.com>
2021-02-05 13:57:24 -0300

    cache

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,24 @@
 
 self.registration.showNotification( 'Notification from service Worker', {} );
 
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+    '/',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/css/help.css',
+    '/src/js/feed.js',
+    '/src/js/material.min.js'
+];
+
 self.addEventListener( 'install', function ( event ) {
     console.log( '[Service Worker] Installing Service Worker ...', event );
+    event.waitUntil(
+        caches.open( CACHE_NAME )
+            .then( function ( cache ) {
+                console.log( 'Opened cache' );
+                return cache.addAll( urlsToCache );
+            } )
+    );
 } );
 
